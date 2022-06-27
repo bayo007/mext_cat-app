e8303fc2ae46a152b0d037de74c36ebbc915df4d
bayo007 <yekeenadebayo01@gmail.com>
2022-06-27 09:30:09 +0100

    commit

--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import DefaultLayout from "../../layouts/Default";
-import Link from "next/link"; 
 
 export default function Home() {
   const router = useRouter();
